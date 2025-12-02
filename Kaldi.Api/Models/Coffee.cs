629a9606f369981bc83778f896f06d5c21f5e1e8
Gabriel Cruz <gabriel.cruz232@gmail.com>
2025-12-02 19:09:19 -0300

    add Api project and db entity Coffee

---
+++ Kaldi.Api/Models/Coffee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kaldi.Api.Models;
+
+public class Coffee
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public DateTime? ArrivalDate { get; set; }
+    public DateTime? RoastDate { get; set; }
+    public string? SensoryProfile { get; set; }
+    public string? Variety { get; set; }
+    public string? Region { get; set; }
+    public string? ProcessingMethod { get; set; }
+    public string? Producer { get; set; }
+    public int? Altitude { get; set; }
+    public string? Vendor { get; set; }
+    public string? Roast { get; set; }
+    public float? Weight { get; set; }
+    public string? Description { get; set; }
+}
---
+++ Kaldi.Api/Services/KaldiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Kaldi.Api.Models;
+
+namespace Kaldi.Api.Services;
+
+public class KaldiContext : DbContext
+{
+    public DbSet<Coffee> Coffees { get; set; }
+
+    public KaldiContext(DbContextOptions<KaldiContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coffee>().ToTable("Coffee");
+    }
+}
